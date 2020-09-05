4285fe55104e0d5c2478fe07fe21170ea5f90c7a
Nikita <nikitajz@users.noreply.github.com>
2020-09-06 01:18:22 +0300

    refactor predict.py without config.json reference

--- src/predict.py
+++ src/predict.py
@@ -1,5 +1,4 @@
 import logging
-import os
 from pathlib import Path
 
 import joblib
@@ -8,45 +7,44 @@ import torch
 from catalyst.dl import SupervisedRunner
 from torch.nn.functional import softmax
 
-from src.config.config_base import ModelArgs, TrainingArgs
-from src.config.hf_argparser import load_or_parse_args
 from src.data.data_loaders import get_test_data_loader
 from src.model import get_model
 
 
 def run_prediction():
-    logger.info("Running prediction")
-    model_args, training_args = load_or_parse_args((ModelArgs, TrainingArgs), verbose=True)
+    artifacts_dir = Path("/kaggle/input/landmarks-model-plus")
+    checkpoint_path = artifacts_dir / "best.pth"
+    label_encoder_path = artifacts_dir / "label_encoder.jl"
+    num_classes_path = artifacts_dir / "num_classes.jl"
+    data_path = Path("/kaggle/input/landmark-recognition-2020")
+    batch_size = 64
+    num_workers = 6
+    model_name = "efficientnet-b0"
+    device = torch.device('cuda:0')
 
-    submission_df = pd.read_csv(training_args.data_path / "sample_submission.csv")
+    logger.info("Running prediction")
+
+    submission_df = pd.read_csv(data_path / "sample_submission.csv")
 
     logger.info('Loading test dataloader')
     test_loader = get_test_data_loader(submission_df,
-                                       image_dir=training_args.data_path,
-                                       batch_size=training_args.batch_size * 2,
-                                       num_workers=training_args.num_workers)
-
-    is_kernel = os.environ.get("KAGGLE_KERNEL_RUN_TYPE") is not None
-    artifacts_dir = Path("/kaggle/input/landmarks-model-plus") if is_kernel else Path(training_args.log_dir)
-    device = torch.device(training_args.device)
+                                       image_dir=data_path,
+                                       batch_size=batch_size,
+                                       num_workers=num_workers)
 
     logger.info('Loading model...')
-    checkpoint_path = artifacts_dir / "best.pth"
-    num_classes = joblib.load(filename=artifacts_dir/training_args.num_classes_filename)
-    model = get_model(model_name=model_args.model_name, n_classes=num_classes, pretrained=False)
+    num_classes = joblib.load(filename=num_classes_path)
+    model = get_model(model_name=model_name, n_classes=num_classes, pretrained=False)
 
     runner = SupervisedRunner(device=device)
     output = torch.cat([softmax(pred[runner.output_key], dim=1) for pred in
                         runner.predict_loader(loader=test_loader, model=model, resume=str(checkpoint_path))])
     probs, preds = torch.max(output, dim=1)
 
-    predictions, scores = preds.cpu().numpy(), probs.cpu().numpy()
-    del preds, probs
+    label_enc = joblib.load(label_encoder_path)
+    labels = label_enc.inverse_transform(preds.cpu().numpy())
 
-    label_enc = joblib.load(artifacts_dir/training_args.label_encoder_filename)
-    labels = label_enc.inverse_transform(predictions)
-
-    submission_df['landmarks'] = [f'{label} {score}' for label, score in zip(labels, scores)]
+    submission_df['landmarks'] = [f'{label} {score}' for label, score in zip(labels, probs.cpu().numpy())]
     submission_df.to_csv('submission.csv', index=False)
 
 
