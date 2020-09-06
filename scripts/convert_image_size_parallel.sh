7db866c93df13599e6c821864cc3d0cecbe4c36f
Nikita <nikitajz@users.noreply.github.com>
2020-09-06 04:39:17 +0300

    update convert resolution script

--- scripts/convert_image_size_parallel.sh
+++ scripts/convert_image_size_parallel.sh
@@ -2,11 +2,11 @@
 # default Ubuntu `parallel` is part of `moreutils` package and quite limited. I've used instead GNU `parallel`.
 # sudo apt-get remove moreutils && sudo apt-get install parallel
 
-res=x64  # resolution, square e.g. 256x256
-output_dir=$res  # alternatively ../x256 if it should be placed in the parent dir
+# run inside the original directory
+res=x224  # resolution, square e.g. 256x256
+output_dir=../$res  # alternatively ../x256 if it should be placed in the parent dir
 
-echo "Converting image to resolution $res"
+echo "Converting image to resolution $res, output_dir is $output_dir"
 
-# TODO: echo and mkdir should be done once per folder, not for each file
-# TODO: rewrite to process subfolder (e.g. orig) to skip folders with other converted images
-find . -maxdepth 6 -type f -name '*.jpg' | parallel "echo {}; mkdir -p $output_dir/{//}; convert -geometry $res {} $output_dir/{}"
+# TODO: mkdir should be done once per folder, not for each file
+find . -name '*.jpg' | parallel --bar "mkdir -p $output_dir/{//}; convert -quality 98 -resize \"224x224\" {} $output_dir/{}"
